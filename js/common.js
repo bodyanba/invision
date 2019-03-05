d885617febd699a75e6f79b0d8a992dcf42c9bba
bodyanba <galkinbogdan@gmail.com>
2019-03-05 10:13:45 +0200

    changes

--- js/common.js
+++ js/common.js
@@ -1,3 +1,18 @@
-document.getElementById('checkbox').addEventListener('change', function() {
-	document.getElementById('row').classList.toggle("active")
+var checkbox   = document.getElementById('checkbox');
+var row        = document.getElementById('row');
+var switchWrap = document.getElementById('switch-wrap');
+
+checkbox.addEventListener('change', function() {
+	row.classList.toggle("active");
+	switchWrap.classList.toggle("active");
+});
+document.getElementById('monthly').addEventListener('click', function() {
+	checkbox.checked = false;
+	row.classList.remove("active");
+	switchWrap.classList.remove("active");
+});
+document.getElementById('yearly').addEventListener('click', function() {
+	checkbox.checked = true;
+	row.classList.add("active");
+	switchWrap.classList.add("active");
 });
